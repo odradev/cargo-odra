b576a8756597137f9f00079be177445edea5b0d9
Kuba Płaskonka <kuba.plaskonka@gmail.com>
2022-06-21 11:15:52 +0200

    odra test first version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::ffi::OsString;
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 use clap::{Parser, Subcommand};
-
+use convert_case::{Case, Casing};
 
 #[derive(Parser)]
 #[clap(name = "cargo")]
@@ -22,7 +26,6 @@ enum OdraSubcommand {
     New(New),
     Build(Build),
     Test(Test),
-    #[clap(subcommand)]
     Generate(Generate),
 }
 
@@ -41,19 +44,18 @@ struct Build {
 
 }
 
-#[derive(clap::Args)]
+#[derive(clap::Args, Debug)]
 struct Test {
-
+    #[clap(value_parser, long, short)]
+    backend: Option<String>,
+    #[clap(value_parser, long, short)]
+    passthrough: Option<Vec<OsString>>
 }
 
-#[derive(clap::Subcommand)]
-enum Generate {
-    Contract(Contract),
-}
-
-#[derive(clap::Args)]
-struct Contract {
-
+#[derive(clap::Args, Debug)]
+struct Generate {
+    #[clap(value_parser, long, short)]
+    contract_name: String,
 }
 
 fn main() {
@@ -62,20 +64,80 @@ fn main() {
         OdraSubcommand::Build(_) => {
             println!("Build!");
         }
-        OdraSubcommand::Test(_) => {
-            println!("Test!");
-        }
-        OdraSubcommand::Generate(generate) => {
-            match generate {
-                Generate::Contract(_) => {
-                    println!("Contract!");
-                }
+        OdraSubcommand::Test(test) => {
+            match test.backend {
+                None => {test_mock_vm(&test);}
+                Some(_) => {test_backend(&test);}
             }
         }
-        OdraSubcommand::New(_) => {
-            Command::new("cargo")
-                .args(["generate", "odradev/odra-template"])
-                .exec();
+        OdraSubcommand::Generate(generate) => {
+            generate_contract(&generate);
         }
+        OdraSubcommand::New(_) => {
+            new_project();
+        }
+    }
+}
+
+fn test_backend(test: &Test) {
+    let backend = test.backend.clone().unwrap();
+    println!("Test with backend {}", backend);
+    let test_args = get_test_args(test);
+    println!("With passthrough params:");
+    test_args.iter().for_each(|s| println!("{}", s));
+}
+
+fn test_mock_vm(test: &Test) {
+    let test_args = get_test_args(test);
+
+    Command::new("cargo")
+    .args(test_args)
+    .exec();
+}
+
+fn get_test_args(test: &Test) -> Vec<&str> {
+    let mut test_args = vec!["test"];
+    match &test.passthrough {
+        None => {}
+        Some(passthrough) => {
+            let passthrough = passthrough.first().unwrap().as_os_str().to_str().unwrap();
+            let mut vec: Vec<&str> = passthrough.split(' ').collect();
+            test_args.append(&mut vec);
+        }
+    }
+    test_args
+}
+
+fn new_project() {
+    Command::new("cargo")
+        .args(["generate", "odradev/odra-template"])
+        .exec();
+}
+
+fn generate_contract(generate: &Generate) {
+    println!("Contract: {}", generate.contract_name);
+    let contract_body = attohttpc::get("https://raw.githubusercontent.com/odradev/odra-template/master/src/flipper.rs").send().unwrap().text().unwrap();
+    let contract_body = contract_body.replace("Flipper", generate.contract_name.to_case(Case::UpperCamel).as_str());
+    let contract_body = contract_body.replace("flipper", generate.contract_name.to_case(Case::Lower).as_str());
+    fs::write(("src/".to_string() + &generate.contract_name + ".rs").as_str(), contract_body).unwrap();
+    let mut lib_rs = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("src/lib.rs")
+        .unwrap();
+    let mod_line = "pub mod ".to_string() + &generate.contract_name + ";";
+    let use_line = "pub use ".to_string() + &generate.contract_name + "::" + &generate.contract_name.to_case(Case::UpperCamel) + ";";
+    writeln!(lib_rs).unwrap();
+    writeln!(lib_rs, "{}", mod_line).unwrap();
+    writeln!(lib_rs, "{}", use_line).unwrap();
+    writeln!(lib_rs).unwrap();
+    lib_rs.flush().unwrap();
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_smth() {
+        assert!(true);
     }
 }
