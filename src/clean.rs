20fb77f9172f900258ae617f476e12511b7bedad
Kuba Płaskonka <kuba.plaskonka@gmail.com>
2022-07-19 09:39:51 +0200

    cargo odra clean

---
+++ src/clean.rs
@@ -0,0 +1,30 @@
+use std::os::unix::prelude::CommandExt;
+use std::path::PathBuf;
+use std::process::Command;
+
+pub struct Clean {}
+
+impl Clean {
+    pub fn new() -> Clean {
+        Clean {}
+    }
+
+    pub fn clean(&self) {
+        for folder in glob::glob(".backend*").unwrap().flatten() {
+            Clean::rm_rf(folder);
+        }
+
+        for folder in glob::glob(".builder*").unwrap().flatten() {
+            Clean::rm_rf(folder);
+        }
+
+        println!("Running cargo clean...");
+        Command::new("cargo").args(["clean"]).exec();
+    }
+
+    fn rm_rf(folder: PathBuf) {
+        rm_rf::ensure_removed(folder.clone())
+            .unwrap_or_else(|_| panic!("Couldn't remove {}", folder.display()));
+        println!("Removing {}...", folder.display());
+    }
+}
--- src/odra_toml.rs
+++ src/odra_toml.rs
@@ -1,6 +1,9 @@
 use serde_derive::Deserialize;
 use std::collections::HashMap;
-use std::fs;
+use std::path::Path;
+use std::{fs, process};
+
+const ODRA_TOML_FILENAME: &str = "Odra.toml";
 
 #[derive(Deserialize, Debug, Clone)]
 pub(crate) struct OdraConf {
@@ -16,12 +19,19 @@ pub(crate) struct Contract {
 }
 
 pub(crate) fn load_odra_conf() -> OdraConf {
-    let odra_conf = fs::read_to_string("Odra.toml");
+    let odra_conf = fs::read_to_string(ODRA_TOML_FILENAME);
     match odra_conf {
         Ok(conf_file) => toml::from_str(conf_file.as_str()).unwrap(),
         Err(_) => {
             panic!("Odra.toml file is missing. Is Odra initialized?")
         }
+    }
+}
+
+pub fn assert_odra_toml() {
+    if !Path::new(ODRA_TOML_FILENAME).exists() {
+        println!("This command can be executed only in folder with Odra project.");
+        process::exit(1);
     }
 }
 
