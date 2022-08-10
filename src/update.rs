ec758692590773f6119b5cbb6d59c6511533c75e
Kuba Płaskonka <kuba@odra.dev>
2022-08-10 11:27:30 +0200

    Cargo odra update.

---
+++ src/update.rs
@@ -0,0 +1,42 @@
+use crate::odra_toml::OdraConf;
+use crate::{command, Backend, UpdateCommand};
+use prettycli::{error, info};
+use std::process::exit;
+
+pub fn update(update_command: UpdateCommand) {
+    if update_command.backend.is_some() {
+        let backends = OdraConf::load().backends.unwrap();
+        let backend = backends
+            .get(&update_command.backend.unwrap())
+            .unwrap_or_else(|| {
+                error("No such backend");
+                exit(1);
+            });
+        update_builder(backend);
+    } else {
+        update_everything();
+    }
+}
+
+fn update_builder(backend: &Backend) {
+    info(&format!(
+        "Running cargo update for {} builder...",
+        backend.name()
+    ));
+    command::cargo(backend.builder_path(), vec!["update"]);
+}
+
+fn update_project() {
+    info("Running cargo update for project...");
+    command::cargo(".".to_string(), vec!["update"]);
+}
+
+fn update_everything() {
+    update_project();
+    let backends = OdraConf::load().backends;
+    if let Some(..) = backends {
+        for (_key, backend) in backends.unwrap() {
+            update_builder(&backend);
+        }
+    }
+}
