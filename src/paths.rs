0236e3077227c2fbc01f0f63e1b6dc1f689f224d
Kuba Płaskonka <kuba@odra.dev>
2024-01-30 12:33:05 +0100

    Discover other than default target dirs

--- src/paths.rs
+++ src/paths.rs
@@ -1,6 +1,9 @@
 //! Paths utils.
 
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 use convert_case::{Boundary, Case, Casing};
 
@@ -11,9 +14,39 @@ pub fn wasm_file_name(contract_name: &str) -> PathBuf {
 
 /// Returns *.wasm file path in target directory.
 pub fn wasm_path_in_target(contract_name: &str, project_root: PathBuf) -> PathBuf {
+    // extract target dir
+    let target_dir = get_build_target_dir();
     project_root
-        .join("target/wasm32-unknown-unknown/release")
+        .join(format!(
+            "{}/wasm32-unknown-unknown/release",
+            target_dir.display()
+        ))
         .join(wasm_file_name(contract_name))
+}
+
+fn get_build_target_dir() -> PathBuf {
+    let cmd = "cargo config get build.target-dir -Z unstable-options";
+    let output = Command::new("sh").arg("-c").arg(cmd).output();
+
+    if output.is_err() {
+        return PathBuf::from("target");
+    }
+
+    // convert output to string
+    let target_dir = String::from_utf8(output.unwrap().stdout).unwrap();
+
+    // output is in format build.target-dir = "../target"
+    // convert it to PathBuf
+    let target_dir = target_dir.split('=').collect::<Vec<&str>>();
+    let target_dir = target_dir.get(1);
+
+    if target_dir.is_none() {
+        return PathBuf::from("target");
+    }
+
+    let target = target_dir.unwrap().trim().to_string().replace('\"', "");
+
+    PathBuf::from(target)
 }
 
 /// Returns *.wasm file path in wasm directory.
